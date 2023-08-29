6f904ce371b77aa511bc41a9530c18a75bb67e75
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2023-08-29 07:01:18 +0700

    init

---
+++ belajar-nodejs-npm/package.js
@@ -0,0 +1,9 @@
+import { sayHello , sum} from "belajar-nodejs-npm-library-farhan";
+import { max, min } from "../belajar-nodejs-npm-library/number.js";
+
+console.info(sayHello("Farhan"))
+
+
+console.info(sum([10,10,80]))
+console.info(min(10,20))
+console.info(max(10,20))
